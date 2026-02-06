34d50a00eee974f006b9849cb3004531517b8932
Albert Lukmanov <albert.lukmanov99@gmail.com>
2026-02-06 10:11:02 -0300

    Fix: Point Package.swift to remote v1.0.0 binary

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "BabLanguageSDK",
             url: "https://github.com/etonealbert/bab-language-kmp/releases/download/v1.0.0/BabLanguageSDK.xcframework.zip",
-            checksum: "PLACEHOLDER_CHECKSUM_REPLACE_AFTER_UPLOAD"
+            checksum: "6cadf68ce275971221e69d0f216a6cb904e5e49f97c16d64a6ea46c075449dfe"
         ),
     ]
 )
